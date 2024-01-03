62426832bb68cf6e71d1c52912d3e0ea1525a826
Yulia <sivonenkou@gmail.com>
2024-01-04 01:10:57 +0200

    array

---
+++ 09-array.js
@@ -0,0 +1,16 @@
+// Напиши скрипт який рахує суму єлементів двух масивів
+
+const array1 = [5, 10, 15, 20];
+const array2 = [10, 20, 30];
+
+let total = 0;
+
+const numbers = array1.concat(array2);
+
+for (const number of numbers) {
+    total += number;
+
+}
+
+console.log(total);
+
