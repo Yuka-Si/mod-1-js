e6c46cc3fb57ca77cbddb34efac8290ba45ecd92
Yulia <sivonenkou@gmail.com>
2024-01-03 17:17:09 +0200

    payment

--- 02-payment.js
+++ 02-payment.js
@@ -1,3 +1,30 @@
 // Порахувати загальну кількість покупок в корзині
 
 
+const card = [54, 28, 105, 70, 92, 17, 120];
+// 2 зробити змінну total до цикла
+let total = 0;
+
+// // 1 перебрати масив
+// for (let i = 0; i < card.length; i += 1) {
+//     // console.log(card[i]);
+//     // 3 кожен елемент додати до total
+//     total += card[i];
+// }
+// console.log('Total: ', total);
+
+// *************** другий варіант*******
+
+for (const value of card) {
+    total += value;
+}
+console.log('Total: ', total);
+
+
+
+
+
+
+
+
+
